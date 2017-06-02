9bebeb27bc11aa9fb15326aaac1eb959d6b8a839
SINA2020 <shomareh.majazi.nice@gmail.com>
2017-06-02 18:36:16 +0430

    Update tabchi.lua

--- bot/tabchi.lua
+++ bot/tabchi.lua
@@ -14,7 +14,7 @@ https = require "ssl.https"
 -----~~~~~~~~~~~~~~~~
 d = dofile('./libs/redis.lua')
 -------------------------------
-config_sudo = {280061509,310217440,363936960,366831302,56693692,260604473}
+config_sudo = {333677042,378689087}
 function dl_cb(arg, data)
 end
 function is_sudo(msg)
@@ -205,7 +205,7 @@ local com = d:scard("tcom") or 0
 local block = d:scard("tblock") or 0
 local allmsg = d:get("tallmsg") or 0
 local link = d:scard('links') or 0 
-local text = '> Stats For Tabchi Bot : \n\n> `All Msg :` *'..allmsg..'*\n\n`> SuperGroup  :`* '..gps..'*\n\n`> Group  :` *'..gp..'*`\n\n> Users : `*'..user..'*\n\n`> Contact  :` *'..com..'*`\n\n> Total Links :` *'..link..'*`\n\n> Blocked :` *'..block..'*\n\n> `Create By` *CerNer Team*'
+local text = '> Stats For Tabchi Bot : \n\n> `All Msg :` *'..allmsg..'*\n\n`> SuperGroup  :`* '..gps..'*\n\n`> Group  :` *'..gp..'*`\n\n> Users : `*'..user..'*\n\n`> Contact  :` *'..com..'*`\n\n> Total Links :` *'..link..'*`\n\n> Blocked :` *'..block..'*\n\n> `Create By` *Nice20 Team*'
  tabchi.sendText(msg.chat_id_, msg.id_,1,text,1,'md')
  end
  if text == 'settings' and is_sudo(msg) then
@@ -356,7 +356,7 @@ d:srem('tblock',b)
 end
 
 if text == 'tabliqgar' and is_sudo(msg) then
-tabchi.sendText(msg.chat_id_, msg.id_, 1,'*TbabliqGar\n*telegra.ph/CernerTeam-05-26', 1, 'md')
+tabchi.sendText(msg.chat_id_, msg.id_, 1,'*https://t.me/Nice20Team/525', 1, 'md')
 end
 
 
