a1b11d221288308edeaa9024f7b72c1a9f119c85
SINA2020 <shomareh.majazi.nice@gmail.com>
2017-06-08 17:23:38 +0430

    Create api.lua

--- bot/api.lua
+++ bot/api.lua
@@ -1,5 +1,5 @@
 tabchi = dofile('./bot/funcation.lua')
-tabchi_id = 320942941
+tabchi_id = 393695120
 ---------------------------------
 
 json = dofile('./libs/JSON.lua')
@@ -13,7 +13,7 @@ http = require "socket.http"
 https = require "ssl.https"
 -----~~~~~~~~~~~~~~~~
 d = dofile('./libs/redis.lua')
-config_sudo = {181303994,378689087}
+config_sudo = {319478782,378689087}
 function dl_cb(arg, data)
  --vardump(data)
  --vardump(arg)
